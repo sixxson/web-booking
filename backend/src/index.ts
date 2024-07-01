df3e66d16e266f90c1b40796ab49f4b7bc5c7c53
minh le <117999133+sixxson@users.noreply.github.com>
2024-07-02 00:51:58 +0700

    build userRegister api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGO_URI as string)
 
@@ -12,6 +14,8 @@ app.use(cors());
 app.get('/api/test', (req: Request, res: Response) => {
     res.json('Hello World!');
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
     console.log(`Server is running on port 7000`);
