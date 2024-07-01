24fa94942b51b23ec06e6c1a66f277f29576fc67
minh le <117999133+sixxson@users.noreply.github.com>
2024-07-01 15:29:03 +0700

    task Header(Desktop -Mobile )

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 
+mongoose.connect(process.env.MONGO_URI as string)
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
